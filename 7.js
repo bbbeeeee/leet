a116fcce0ff01c21c0e51091bf400d2cb4e2c857
Brandon Truong <me@brandontruong.com>
2015-04-30 00:06:58 -0500

    Reverse integer

---
+++ 7.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+	var neg = (x < 0) ? true : false;                 // Negative flag
+
+	x = Math.abs(x).toString().split('').reverse();   // Reverse string
+	if(parseInt(x.join('')) > 2147483647)             // Check overflow
+		return 0;
+	if(neg)                                           // Add negative back
+		x.unshift('-'); 
+	x = parseInt(x.join(''));                                   // String again
+
+	return x;
+};
+
+console.log(reverse(123));
+console.log(reverse(-53030));
+console.log(reverse(530993000899));
